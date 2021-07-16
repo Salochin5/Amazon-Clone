b58858b5232383b31e6a0d72ba4eb453fbb8f0da
Salochin <nickysalochin@gmail.com>
2021-07-16 19:08:40 +0300

    install firebase tools

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+ apiKey: "AIzaSyCXZy7pJaLIKuVJOgZimztB3WTWXp3-mAU",
+ authDomain: "clone-3fd00.firebaseapp.com",
+ projectId: "clone-3fd00",
+ storageBucket: "clone-3fd00.appspot.com",
+ messagingSenderId: "1084225138841",
+ appId: "1:1084225138841:web:aeac4388d75c8eb163c808",
+ measurementId: "G-92HJSTFBBQ"
+};
